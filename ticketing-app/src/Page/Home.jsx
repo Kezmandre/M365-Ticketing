711bf12e2ef180c1058dda2db394dc05fdcca6f1
Quadri <kazeemquadri75@gmail.com>
2024-12-18 15:39:03 +0000

    loader

--- ticketing-app/src/Page/Home.jsx
+++ ticketing-app/src/Page/Home.jsx
@@ -1,27 +1,42 @@
-
-import React,{useState} from 'react'
-import EventIntro from '../componets/EventIntro'
-import RegistrationForm from '../componets/RegistrationForm'
-
+import React, { useState, useEffect } from "react";
+import EventIntro from "../componets/EventIntro";
+import RegistrationForm from "../componets/RegistrationForm";
+import Loading from "../componets/Loading";
 
 const Home = () => {
+  const [loading, setLoading] = useState(true); // State for loading page
+  const [currentPage, setCurrentPage] = useState(1); // State for managing pages
 
-    const [currentPage, setCurrentPage] = (useState(1))
+  // Navigate to EventIntro after 5 seconds
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 5000);
 
-    const goToNext=()=>{
-        setCurrentPage(currentPage + 1)
-    }
+    return () => clearTimeout(timer); // Cleanup timer on component unmount
+  }, []);
 
+  const goToNext = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
+  if (loading) {
+    return (
+      <div className="loading-screen">
+        <Loading/>
+      </div>
+    );
+  }
 
   return (
     <div>
-        {currentPage === 1 ? (
-            <EventIntro OnNext={goToNext}/>
-        ):(
-         <RegistrationForm/>   
-        )}
+      {currentPage === 1 ? (
+        <EventIntro OnNext={goToNext} />
+      ) : (
+        <RegistrationForm />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- ticketing-app/src/componets/Loading.jsx
+++ ticketing-app/src/componets/Loading.jsx
@@ -4,11 +4,11 @@ import Logo from "../images/logo.jpeg";
 const Loading = () => {
   return (
     <div className="w-[390px] mx-auto h-[600px]">
-      <div className="w-full h-full mt-10 ">
-        <div className="w-[100px] mx-auto h-[100px] animate-spin my-4">
+      <div className="w-full h-full mt-[200px] ">
+        <div className="w-[80px] mx-auto h-[80px] animate-spin my-4">
           <img src={Logo} alt="" className="w-full h-full rounded-full" />
         </div>
-        <h2 className="font-serif font-semibold text-center text-xl pb-3">
+        <h2 className="font-serif font-semibold  text-center text-xl pb-3">
           M365 Denim Party Registration Page
         </h2>
         <p className="text-lg text-center">
